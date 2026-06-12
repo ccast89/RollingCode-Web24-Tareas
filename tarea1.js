a9230f9e35245d45324b1da333578c47cf4a1685
ccast89 <carlacast09@gmail.com>
2026-06-12 15:01:00 -0300

    se resuelve la consigna número 2

--- tarea1.js
+++ tarea1.js
@@ -19,3 +19,37 @@ if (num1 > num2) {
 } else {
   console.log("Ambos números son iguales");
 }
+/* 📝 Ejercicio 2: Calcular promedio de notas
+Pide al usuario tres notas con decimales.  
+
+Convierte las entradas a números decimales.
+
+Calcula el promedio de las tres notas usando operadores aritméticos.  
+
+Muestra el promedio en la consola.  
+
+Usa un condicional para indicar si el promedio es suficiente para aprobar (ejemplo: mayor o igual a 6). */
+
+let nota1 = prompt("Ingrese la primera nota con decimales");
+let nota2 = prompt("Ingrese la segunda nota con decimales");
+let nota3 = prompt("Ingrese la tercera nota con decimales");
+
+nota1 = parseFloat(nota1);
+nota2 = parseFloat(nota2);
+nota3 = parseFloat(nota3);
+
+let promedio = (nota1 + nota2 + nota3) / 3;
+
+console.log(`Tu promedio es de: ${promedio}`);
+
+if (promedio >= 8) {
+  console.log(
+    "😁 Excelente!! Aprobaste y estás entre los mejores de la clase!!",
+  );
+} else if (promedio >= 6) {
+  console.log("😊Muy bien!!! Aprobaste!!!");
+} else {
+  console.log(
+    "😣No te alcanzó para aprobar... A estudiar para la recuperación!",
+  );
+}
