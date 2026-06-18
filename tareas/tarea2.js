3a8073653767e37bda74078263b23f3b44c4b756
ccast89 <carlacast09@gmail.com>
2026-06-18 17:31:02 -0300

    se crea una carpeta para agrupar las tareas y el archivo js de la tarea 2 con las respectivas consignas

---
+++ tareas/tarea2.js
@@ -0,0 +1,11 @@
+/* Trabajemos con String
+Se necesita un script que solicite al usuario escribir una frase o texto y luego ingresar una palabra que quiera buscar. En cualquier caso (sea que encuentre la palabra o no) el programa debe devolver un mensaje informando el resultado (Usar emojis también).
+Usar los métodos aprendidos en la clase para encontrar la solución. */
+
+/* Usando Math
+Crear un script que simule el juego de Adivina el número. Primero se genera un número aleatorio entre 1 y 10 que el usuario no debe saber, Luego el usuario ingresa un número entre 1 y 10 tratando de adivinar. Devolver un mensaje si ganó o perdió. */
+
+/* Juguemos con Switch
+En un restaurante los clientes pueden pedir menú de carne, pescado o verdura. Si pide carne se le ofrecerá como bebida vino tinto, si pide pescado se le ofrecerá vino blanco y si pide verdura se le ofrecerá agua.
+Si no elige el menú de la lista aparecerá la frase elija carne, pescado o verdura.
+Usar emojis */
