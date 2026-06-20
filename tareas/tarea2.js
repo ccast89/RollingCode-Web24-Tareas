ab80064a1be154b85ff4cb7c1ffa589fb4e960e8
ccast89 <carlacast09@gmail.com>
2026-06-20 10:48:16 -0300

    Se resuelve la segunda consigna

--- tareas/tarea2.js
+++ tareas/tarea2.js
@@ -13,10 +13,22 @@ if (frase.includes(palabra)) {
 } else {
   console.log(`La palabra ${palabra} no está incluída en la frase 😣`);
 }
-
 /* Usando Math
 Crear un script que simule el juego de Adivina el número. Primero se genera un número aleatorio entre 1 y 10 que el usuario no debe saber, Luego el usuario ingresa un número entre 1 y 10 tratando de adivinar. Devolver un mensaje si ganó o perdió. */
 
+alert("Hola!!!😁 Jugamos??? Tenés que adivinar un número del 1 al 10");
+
+let numeroRandom = Math.ceil(Math.random() * 10);
+let numeroElegido = prompt("Ingresá un número");
+
+numeroElegido = Number(numeroElegido);
+console.log(numeroRandom);
+
+if (numeroElegido == numeroRandom) {
+  alert("Felicidades!!!! Adivinaste el número 🎉");
+} else {
+  alert("Lo siento 😣, no acertaste, suerte para la próxima🍀");
+}
 /* Juguemos con Switch
 En un restaurante los clientes pueden pedir menú de carne, pescado o verdura. Si pide carne se le ofrecerá como bebida vino tinto, si pide pescado se le ofrecerá vino blanco y si pide verdura se le ofrecerá agua.
 Si no elige el menú de la lista aparecerá la frase elija carne, pescado o verdura.
