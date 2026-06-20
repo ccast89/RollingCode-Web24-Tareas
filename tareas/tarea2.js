3b6a0784ed3b88b94f7bc01c35f120b80d7851bb
ccast89 <carlacast09@gmail.com>
2026-06-20 09:56:54 -0300

    Se resuelve la primera consigna

--- tareas/tarea2.js
+++ tareas/tarea2.js
@@ -1,6 +1,18 @@
 /* Trabajemos con String
 Se necesita un script que solicite al usuario escribir una frase o texto y luego ingresar una palabra que quiera buscar. En cualquier caso (sea que encuentre la palabra o no) el programa debe devolver un mensaje informando el resultado (Usar emojis también).
 Usar los métodos aprendidos en la clase para encontrar la solución. */
+let frase = prompt("Por favor, ingrese una frase o un texto");
+let palabra = prompt(
+  "Por favor, indique la palabra que desea buscar dentro de la frase o texto previamente ingresado",
+);
+frase = frase.trim().toLowerCase();
+palabra = palabra.trim().toLowerCase();
+
+if (frase.includes(palabra)) {
+  console.log(`La palabra ${palabra} está incluída en la frase 😁`);
+} else {
+  console.log(`La palabra ${palabra} no está incluída en la frase 😣`);
+}
 
 /* Usando Math
 Crear un script que simule el juego de Adivina el número. Primero se genera un número aleatorio entre 1 y 10 que el usuario no debe saber, Luego el usuario ingresa un número entre 1 y 10 tratando de adivinar. Devolver un mensaje si ganó o perdió. */
