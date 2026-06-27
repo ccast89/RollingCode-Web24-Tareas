56768c87a96895719287f70ef3c386dc4c2c0edd
ccast89 <carlacast09@gmail.com>
2026-06-27 20:03:59 -0300

    Después de muchas horas, se resuelve el ejercicio número 2 de la tarea 4

--- tareas/tarea4.js
+++ tareas/tarea4.js
@@ -3,27 +3,70 @@ Consigna: Imagina que quieres ahorrar para un objetivo de $1000. El programa deb
 Desafío lógico: ¿Qué pasa si el usuario ingresa un número negativo? Añade una validación para que no sume montos inválidos.
 Objetivo: Manejo de acumuladores con condiciones de salida basadas en metas. */
 
+console.log("EJERCICIO Nº 1 (TAREA 4)- ACUMULADOR DE DINERO CON TOPE");
+
+let objetivo = 1000;
+let totalAhorrado = 0;
+
+alert(
+  `😊Hola!! Cómo estás??\nEstoy aquí para informarte cuanto vas ahorrando!\n💰En tu alcancía hay $${totalAhorrado} por ahora`,
+);
+
+while (totalAhorrado < objetivo) {
+  let ingreso = prompt(`Ingresa el monto que vas a ahorrar hoy`);
+
+  if (ingreso === null) {
+    alert(
+      "Parece que el chanchito no va a engordar hoy 😣\nHasta la próxima!! 💸",
+    );
+
+    break;
+  }
+
+  let ahorroDiario = Number(ingreso);
+  if (ahorroDiario <= 0 || isNaN(ahorroDiario)) {
+    alert(
+      "No ingresasaste un monto válido, esta alcancía solo recibe dinero 🤑!!\nIntentá de nuevo",
+    );
+  } else {
+    totalAhorrado = totalAhorrado + ahorroDiario;
+    alert(`Tu total ahorrado hasta ahora es $${totalAhorrado} 💵💵💵`);
+  }
+
+  console.log(`💰En tu alcancía hay $${totalAhorrado} por ahora`);
+}
+
+if (totalAhorrado >= objetivo) {
+  console.log(`🥳🎉¡Objetivo alcanzado! Total final: $${totalAhorrado}`);
+  alert(`🥳🎉¡Objetivo alcanzado!\n💲💲💲Total final: $${totalAhorrado}`);
+} else {
+  alert("Sin esfuerzo no vas a llegar a tu meta 😉");
+}
 /* 2. El "Triángulo de Asteriscos" (Ciclo for anidado)
 Consigna: Utiliza dos bucles for (uno dentro de otro) para imprimir un triángulo de asteriscos en la consola con una altura de 5 líneas:
-\*
-\**
-\***
-\****
-\*****
+*
+**
+***
+****
+*****
 Desafío lógico: El bucle externo controla las filas y el interno controla cuántos asteriscos imprimir en cada fila. 
 Objetivo: Dominar la complejidad de los bucles anidados, fundamental para entender estructuras de datos complejas en el futuro.  */
-
+console.log("EJERCICIO Nº 2 (TAREA 4)- EL TRIÁNGULO DE ASTERISCOS");
 /* 3. El Simulador de "Token de Seguridad" (Ciclo while)
 Contexto: Hoy en día, los sistemas de seguridad nos piden ingresar un código que caduca en segundos o nos limita la cantidad de intentos.
 Consigna: Crea un programa que simule un sistema de autenticación de 2 pasos. El sistema tiene un código fijo (ej: 5555). El usuario tiene un máximo de 3 intentos para ingresarlo correctamente.
 Desafío lógico: Usa un contador que aumente con cada intento fallido y un while que verifique dos condiciones: que el código sea incorrecto y que los intentos sean menores a 3. */
-
+console.log("EJERCICIO Nº 3 (TAREA 4)- EL SIMULADOR DE TOKEN DE SEGURIDAD");
 /* 4. Gestor de "Horas de Estudio" (Ciclo for)
 Contexto: Los estudiantes suelen organizar su semana para cumplir con el curso Fullstack.
 Consigna: Crea un programa donde el usuario ingrese cuántas horas dedicará a estudiar cada día de la semana (7 días). El programa debe usar un ciclo for para solicitar el dato 7 veces, ir sumando las horas en un acumulador y, al final, mostrar el promedio de horas diarias dedicadas al estudio.
 Desafío lógico: ¿Qué pasa si el usuario ingresa un valor negativo? El programa debe ignorar ese dato y pedir que ingrese un número mayor a 0. */
+console.log("EJERCICIO Nº 4 (TAREA 4)- HORAS DE ESTUDIO");
 
 /* 5. Simulador de "Carrito de Descuentos Progresivos" (Ciclo while)
 Contexto: Muchas tiendas online aplican descuentos que dependen de la cantidad de productos en el carrito.
 Consigna: El usuario comienza con un producto. El programa debe preguntar el precio de cada producto y sumarlo. El ciclo while continuará pidiendo productos mientras el usuario escriba "si" para agregar otro. Si el total supera los $5000, el sistema debe imprimir: "¡Has obtenido un 10% de descuento!".
 Desafío lógico: Debes llevar un control del total acumulado en cada iteración y aplicar el descuento solo una vez al final, antes de imprimir el resultado. */
+console.log(
+  "EJERCICIO Nº 5 (TAREA 4)- SIMULADOR DE CARRITO DE DESCUENTOS PROGRESIVOS",
+);
