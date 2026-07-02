101332d609a05191f0db445246e0cc0791dbf3f1
ccast89 <carlacast09@gmail.com>
2026-07-02 17:21:30 -0300

    Se resuelve el ejercicio 3 (for)

--- tareas/tarea5.js
+++ tareas/tarea5.js
@@ -8,38 +8,55 @@
 //3. El Transformador de Textos (for)
 
 /* Dado un array de palabras, crea un nuevo array donde todas las palabras tengan su primera letra en mayúscula y el resto en minúscula.
-```javascript
-const palabras = ["jAvAsCrIpT", "pRoGrAmAcIoN", "eJeRcIcIo"];
-``` */
 
+const palabras = ["jAvAsCrIpT", "pRoGrAmAcIoN", "eJeRcIcIo"];
+
+//ALGORITMO
+/*
+ENTRADA: Array palabras
+PROCESO: recorrer las palabras para convertir la primera letra en mayusculas, las demàs en minusuculas y guardarlas en un array nuevo
+SALIDA: mostrar un mje con el array con las palabras capitalizadas
+*/
+const palabras = ["jAvAsCrIpT", "pRoGrAmAcIoN", "eJeRcIcIo"];
+const palabrasCapitalizadas = [];
+
+for (let i = 0; i < palabras.length; i++) {
+  let palabraMinuscula = palabras[i].toLowerCase();
+
+  let palabraTransformada =
+    palabraMinuscula.at(0).toUpperCase() + palabraMinuscula.substring(1);
+
+  palabrasCapitalizadas.push(palabraTransformada);
+}
+console.log(palabrasCapitalizadas);
 //4. Análisis de Temperaturas (for)
 
 /* Dado un array que representa las temperaturas de la semana, encuentra la **temperatura máxima** y la **temperatura mínima**.
-```javascript
+
 const temperaturas = [18, 22, 15, 28, 12, 25, 19];
-``` */
+
 
 //5. El Codificador Simple (for)
 
 /* Crea un programa que recorra un array de números y genere un nuevo array donde cada número sea multiplicado por sí mismo (el cuadrado), pero **solo si es un número par**. Si es impar, el valor debe permanecer igual.
-```javascript
+
 const datos = [1, 2, 3, 4, 5, 6, 7, 8];
-``` */
+
 
 //6. El Buscador de "La Frase Secreta" (while)
 
 /* Tienes un array de strings. Debes recorrerlo y detenerte apenas encuentres la palabra "objetivo". Cuando la encuentres, imprime su índice y rompe el ciclo inmediatamente.
-```javascript
+
 const palabras = ["manzana", "perro", "objetivo", "casa", "sol"];
-``` */
+
 
 //7. La Batería del Robot (while)
 
 /* Imagina que un robot tiene una batería inicial de 100%. Cada tarea que realiza consume una cantidad aleatoria de energía (entre 5 y 20).
 - **Reto:** Cuenta cuántas tareas logró realizar antes de apagarse.
-```javascript
+
 let bateria = 100;
-``` */
+
 
 //8. La Calculadora de Conversión (function)
 
