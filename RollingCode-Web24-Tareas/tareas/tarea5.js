bdf26fb85bce1a659eec901446a38d56d5af875f
ccast89 <carlacast09@gmail.com>
2026-07-23 20:37:36 -0300

    Se resuelven ejercicios 1,2 y 3 de Tarea 7

---
+++ RollingCode-Web24-Tareas/tareas/tarea5.js
@@ -0,0 +1,106 @@
+/* Ejercicio 1: El Transformador de Datos (map)
+Tienes una lista de temperaturas en grados Celsius. Crea una función llamada convertirAFahrenheit que reciba este array y devuelva un nuevo array con las temperaturas convertidas a Fahrenheit.
+Fórmula: (celsius * 9/5) + 32
+Array inicial: [0, 10, 20, 30, 40] */
+console.log("1. El Transformador de Datos (map)");
+
+const celsius = [0, 10, 20, 30, 40];
+
+const convertirAFahrenheit = (listaDeTemperaturas) =>
+  listaDeTemperaturas.map((temperaturas) => (temperaturas * 9) / 5 + 32);
+
+return convertirAFahrenheit;
+
+console.log("-----------------------");
+
+/* Ejercicio 2: El Filtro de Seguridad (filter)
+Tienes un array de números que representan edades de personas. Crea una función llamada obtenerMayoresDeEdad que use .filter() para devolver un array solo con las edades que sean 18 o más.
+Array inicial: [12, 18, 25, 10, 30, 15] */
+
+console.log("2. El Filtro de Seguridad (filter)");
+
+const edad = [12, 18, 25, 10, 30, 15];
+
+const obtenerMayoresDeEdad = (listaDeEdades) =>
+  listaDeEdades.filter((edad) => edad >= 18);
+
+console.log(obtenerMayoresDeEdad(edades));
+
+console.log("-----------------------");
+/* Ejercicio 3: El Buscador de Códigos (find)
+Tienes un array de strings que representan códigos de productos. Crea una función llamada buscarProducto que reciba el array y un código específico. La función debe usar .find() para retornar el código si existe, o "No encontrado" si no está.
+Array inicial: ["A10", "B20", "C30", "D40"] */
+console.log("3. El Buscador de Códigos (find)");
+
+const stock = ["A10", "B20", "C30", "D40"];
+
+const buscarProducto = (listaDeCodigos, codigoEspecifico) => {
+  let codigoEncontrado = listaDeCodigos.find(
+    (codigo) => codigo === codigoEspecifico,
+  );
+  if (codigoEncontrado === undefined) {
+    return "No encontrado";
+  }
+  return codigoEncontrado;
+};
+
+console.log(buscarProducto(stock, "C30"));
+console.log(buscarProducto(stock, "H07"));
+
+console.log("-----------------------");
+/* Ejercicio 4: El Acumulador Maestro (reduce)
+Tienes un array de números que representan las ventas del día. Crea una función llamada calcularTotalVentas que use .reduce() para sumar todas las ventas y devolver el total.
+Array inicial: [100, 250, 50, 300, 120] */
+console.log("4. El Acumulador Maestro (reduce)");
+
+const ventas = [100, 250, 50, 300, 120];
+
+const calcularTotalVentas = (listaDeVentas) =>
+  listaDeVentas.reduce((accum, monto) => accum + monto, 0);
+
+console.log(calcularTotalVentas(ventas));
+
+console.log("-----------------------");
+/* Ejercicio 5: Gestión de Objetos (El "Inventario Simple")
+Crea un array llamado inventario que contenga 3 objetos. Cada objeto debe representar un producto con las propiedades: nombre (string) y precio (number).
+Crea una función llamada listarPrecios que recorra el array y retorne un nuevo array solo con los precios usando .map().
+Tip: Accede a la propiedad mediante producto.precio. */
+console.log("5 Gestión de Objetos (El 'Inventario Simple')");
+
+const inventario = [
+  { nombre: "Auriculares HyperX", precio: 91300 },
+  { nombre: "Teclado Mecánico", precio: 189990 },
+  { nombre: "Micrófono HyperX", precio: 217990 },
+];
+
+const listarPrecios = (listaProductos) =>
+  listaProductos.map((producto) => producto.precio);
+
+console.log("-----------------------");
+/* Ejercicio 6: El Desafío Combinado (Integración)
+Tienes un array de objetos llamado estudiantes donde cada uno tiene nombre y nota.
+Usa .filter() para obtener solo los estudiantes que aprobaron (nota >= 6).
+A esos estudiantes aprobados, usa .map() para obtener solo sus nombres en un nuevo array.
+Imprime el resultado final.
+const estudiantes = [
+  { nombre: "Ana", nota: 8 },
+  { nombre: "Beto", nota: 4 },
+  { nombre: "Carla", nota: 10 },
+  { nombre: "David", nota: 5 }
+];
+Consejos para alumnos:
+Ejercicio 5 y 6:  Cuando trabajan con objetos, el callback de los métodos (como estudiante => estudiante.nota) es la llave maestra para acceder a la información.
+El orden importa: En el ejercicio 6  .filter() primero "limpia" la lista y luego .map() "transforma" lo que quedó. */
+console.log("6. El Desafío Combinado (Integración)");
+
+const estudiantes = [
+  { nombre: "Ana", nota: 8 },
+  { nombre: "Beto", nota: 4 },
+  { nombre: "Carla", nota: 10 },
+  { nombre: "David", nota: 5 },
+];
+
+const estudiantesAprobados = (listaDeEstudiantes) =>
+  listaDeEstudiantes
+    .filter((estudiante) => estudiante.nota >= 6)
+    .map((estudiante) => estudiante.nombre);
---
+++ RollingCode-Web24-Tareas/tareas/tarea7.js
@@ -0,0 +1,92 @@
+/* Ejercicio 1: "La Cuenta Regresiva de Despegue" (setInterval y DOM)
+Consigna: Crea una página web con un título grande que diga "10" y un botón que diga "Iniciar Cuenta Regresiva". Al hacer clic en el botón, debe comenzar un contador hacia atrás de 1 en 1 segundo hasta llegar a 0. Cuando llegue a 0, el texto del título debe cambiar a "¡Despegue! 🚀" y el contador debe detenerse automáticamente.
+
+Tips para resolverlo:
+Guarda el resultado de setInterval en una variable global o externa para poder usar clearInterval cuando el número llegue a 0.
+Recuerda transformar el texto del DOM a número (parseInt) para poder restarle 1 en cada iteración. */
+
+const contador = document.querySelector("#contador");
+let intervalo = null;
+
+const segundero = () => {
+  intervalo = setInterval(() => {
+    contador.textContent = parseInt(contador.textContent) - 1;
+    if (contador.textContent === "0") {
+      clearInterval(intervalo);
+      contador.textContent = "¡Despegue! 🚀 ";
+    }
+  }, 1000);
+};
+
+document.querySelector("#btnIniciar").addEventListener("click", segundero);
+//-------------------
+
+/* Ejercicio 2: "El Semáforo Inteligente" (setTimeout encadenado)
+Consigna: Simula el comportamiento de un semáforo en la pantalla. Crea un círculo (o un cuadro) que cambie de color automáticamente al hacer clic en un botón "Encender Semáforo":
+Empieza en Rojo (durante 3 segundos).
+Pasa automáticamente a Amarillo (durante 2 segundos).
+Pasa automáticamente a Verde (durante 3 segundos).
+
+
+Tips para resolverlo:
+Puedes modificar los estilos usando elemento.style.backgroundColor = "red".
+Para lograr la secuencia, puedes usar setTimeout anidados o funciones estructuradas con tiempos acumulados, asegurándote de cambiar la clase o el estilo del elemento del DOM en cada paso. */
+
+const semaforo = document.querySelector("#semaforo");
+
+const tiempoSemaforo = () => {
+  semaforo.style.backgroundColor = "red";
+  setTimeout(() => {
+    semaforo.style.backgroundColor = "yellow";
+  }, 3000);
+  setTimeout(() => {
+    semaforo.style.backgroundColor = "green";
+  }, 5000);
+  setTimeout(() => {
+    semaforo.style.backgroundColor = "gray";
+  }, 8000);
+};
+
+document
+  .querySelector("#btnSemaforo")
+  .addEventListener("click", tiempoSemaforo);
+
+//----------------
+
+/* Ejercicio 3: "El Mensaje Flash" (setTimeout y clases de Bootstrap)
+Consigna: Crea un botón que diga "Guardar Cambios" y un contenedor de alerta oculto (con la clase d-none de Bootstrap). Al hacer clic en el botón:
+La alerta debe aparecer mostrando el mensaje "¡Datos guardados con éxito!".
+Pasados 3 segundos, la alerta debe desaparecer automáticamente volviendo a ocultarse.
+
+Tips para resolverlo:
+Para mostrar y ocultar la alerta, utiliza classList.remove('d-none') y classList.add('d-none').
+El setTimeout será el encargado de ejecutar la línea que vuelve a agregar la clase d-none tras los 3000 milisegundos. */
+
+const guardar = document.querySelector("#btnGuardar");
+const alerta = document.querySelector("#alerta");
+
+const mostrarAlerta = () => {
+  alerta.classList.remove("d-none");
+  setTimeout(() => (alerta.classList = "d-none"), 3000);
+};
+
+guardar.addEventListener("click", mostrarAlerta);
+//---------------
+//---------------
+
+/* Ejercicio 4: "Buscador de Usuario" (async/await y fetch)
+Consigna: Crea un botón que diga "Cargar Usuario" y una tarjeta vacía en el HTML. Al hacer clic en el botón, utiliza async/await y fetch para consumir la API pública de JSONPlaceholder ([https://jsonplaceholder.typicode.com/users/1](https://jsonplaceholder.typicode.com/users/1)). Una vez que lleguen los datos, pinta el nombre y el correo electrónico dentro de la tarjeta en el DOM.
+
+Tips para resolverlo:
+No olvides que la función asociada al evento del botón debe declararse como async.
+Recuerda el doble paso del fetch: primero obtener la respuesta y luego transformarla a JSON con await respuesta.json().
+Usa template strings (```) para inyectar las propiedades del objeto recibido dentro del innerHTML de la tarjeta */
+//---------------
+
+/*  Ejercicio 5: "Galería de Personajes con Indicador de Carga" (Asincronismo completo)
+Consigna: Crea una aplicación que al presionar un botón "Buscar Personaje" muestre temporalmente el texto "Cargando..." en pantalla. Luego, mediante un fetch a la API de Rick and Morty ([https://rickandmortyapi.com/api/character/5](https://rickandmortyapi.com/api/character/5)), obtén un personaje, borra el texto de carga y muestra su nombre y su imagen en el DOM.
+
+
+Tips para resolverlo:
+Antes de hacer el await fetch(...), actualiza el contenedor #resultado con un texto o un Spinner de Bootstrap para simular la espera.
+Cuando obtengas los datos de la API, reemplaza el contenido del contenedor utilizando las propiedades data.name y data.image del objeto que te devuelve la API. */
